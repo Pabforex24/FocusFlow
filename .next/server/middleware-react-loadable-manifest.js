9870b87ce6fa87c6d492b190a13b7ab3eac33dda
PAB <madzoba2020@gmail.com>
2026-05-05 01:35:22 +0000

    chore: ignorer le dossier .next

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/task/TaskItem.tsx -> @/components/focus/FocusMode\":{\"id\":\"components/task/TaskItem.tsx -> @/components/focus/FocusMode\",\"files\":[\"static/chunks/_app-pages-browser_src_components_focus_FocusMode_tsx.js\"]}}"
